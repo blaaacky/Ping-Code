38a2b33c9bfd8eafda0e1d00103d8c3e57db3cc5
Blaacky <87208336+blaaacky@users.noreply.github.com>
2021-07-09 23:39:37 +0300

    Update ping.js

--- ping.js
+++ ping.js
@@ -1,1 +1,32 @@
-const dd
+const { PREFIX } = require("../../config");
+const Discord = require('discord.js');
+
+
+module.exports = {
+  config: {
+    name: 'ping',
+    description: 'ping command',
+        aliases: ["ping"],
+        usage: '!ping',
+        accessableby: "",
+  },
+  run: async (bot, message, args) => {
+    
+    
+    if(message.author.bot || message.channel.type === "dm") return
+    
+    
+    
+    
+if(message.content.toLowerCase() === `${PREFIX}ping`) {
+  
+  message.lineReply(`> **Looking For Bot Ping..**`).then(message => {
+    message.edit(`> **Bot Ping Is ${bot.ws.ping}Ms!**`)
+  })
+  
+  
+  
+  
+}
+  }
+}
